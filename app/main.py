afc4d7ed8639bc44a39170e83754da6d15a2de15
hectortorres <hector.torres.us@gmail.com>
2025-06-27 19:50:38 -0500

    removed extraneous print statement

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from market_collector import MarketCollector
 def main():
     mc = MarketCollector()
     df = mc.get_events_dataframe()
-    print(df.head())
 
 if __name__ == '__main__':
     main()
